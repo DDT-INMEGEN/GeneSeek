d881b3e57c3a40f563a53cb9b6a5c95097ce9139
sergiolezama <sergio_lezamarv@hotmail.com>
2015-02-22 19:37:54 -0600

    Marcador arreglado

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
-from random import randint
-
-
 
 class PongPaddle(Widget):
     score = NumericProperty(0)
