c888c55158be637d7a7565a93beeb69a1a03da79
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-12-02 15:44:13 -0600

    rudimentary gene summary seek

--- seek.py
+++ seek.py
@@ -2,13 +2,28 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 
+import xml.etree.ElementTree as ET
+from Bio_Eutils import Entrez
+
 
 
 class GeneSeek(Widget):
 
     def do_action(self):
-        print self.ids.textinput.text
-        self.ids.ficha.text = 'Gene such and such, etc.'
+        Entrez.email = 'A.N.Other@example.com'
+        handle = Entrez.esearch(db="gene", retmax=100, term=self.ids.textinput.text + "[sym]")
+        pub_search = Entrez.read(handle)
+        handle = Entrez.efetch(db='gene',  id=pub_search['IdList'], retmax=20, rettype="xml")
+        encoded = handle.read()
+        root = ET.fromstring(encoded)
+
+        summary = ""
+        for i in root:
+            for j in i:
+                if j.tag == 'Entrezgene_summary':
+                    summary += j.text
+
+        self.ids.ficha.text = summary
 
 
 class SeekApp(App):
